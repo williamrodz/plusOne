da65d546496977972b9d5c9d2f5c13bd15324d90
pradosan <pradosan@mit.edu>
2018-05-16 14:33:48 -0400

    months

--- profile.js
+++ profile.js
@@ -1,5 +1,5 @@
 
-var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 var activeGroupUID = null;
 Util.events(document, {
 	// Final initalization entry point: the Javascript code inside this block
